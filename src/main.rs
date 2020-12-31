e8934e06a4ea6c0598e2f43642c875bd056d1638
rchaser53 <tayoshizawa29@gmail.com>
2020-12-31 23:18:51 +0900

    try to handle trk data

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,64 @@
-extern crate gpx;
+extern crate xml;
 
-use std::io::BufReader;
 use std::fs::File;
+use std::io::BufReader;
 
-use gpx::{read, write};
-use gpx::{Gpx, Track, TrackSegment};
+use xml::reader::{EventReader, XmlEvent};
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size).map(|_| INDENT)
+             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+}
 
 fn main() {
-    let file = File::open("./test/wada.gpx").unwrap();
-    let reader = BufReader::new(file);
+    let file = File::open("./test/test.gpx").unwrap();
+    let file = BufReader::new(file);
 
-    let mut gpx: Gpx = read(reader).unwrap();
-    let mut track = &mut gpx.tracks[0];
-    let mut segment = &mut track.segments[0];
-    segment.points.reverse();
-    let mut writer: Vec<u8> = Vec::new();
-    let result = write(&gpx, std::io::stdout());
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    let mut treak = vec![];
+    let mut is_treak = false;
+    for e in parser {
+        if is_treak {
+            match e {
+              Ok(XmlEvent::StartElement { .. }) => {          
+                  depth += 1;
+              }
+              Ok(XmlEvent::EndElement { ref name, .. }) => {
+                  depth -= 1;
+                  if &name.local_name == "trk" {
+                      is_treak = false;
+                  }
+              }
+              Err(e) => {
+                  println!("Error: {}", e);
+                  break;
+              }
+              _ => {}
+            }
+            treak.push(e.clone());
+            continue
+        }
+
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{}+{}", indent(depth), name);                
+                depth += 1;
+                if &name.local_name == "trk" {
+                    is_treak = true;
+                }
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{}-{}", indent(depth), name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+    dbg!(treak);
 }
