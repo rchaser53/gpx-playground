43a62672e41a801c2226d933bd7822799c08a0e8
rchaser53 <tayoshizawa29@gmail.com>
2021-01-01 10:45:27 +0900

    try to reverse trk but failed

--- src/main.rs
+++ src/main.rs
@@ -3,70 +3,67 @@ extern crate xml;
 use std::fs::File;
 use std::io::BufReader;
 
-use xml::reader::{EventReader, XmlEvent};
-use xml::writer::{EventWriter, XmlEvent as WXMLEvent, EmitterConfig, Result};
-use xml::name::OwnedName;
 use xml::attribute::OwnedAttribute;
-
-fn indent(size: usize) -> String {
-    const INDENT: &'static str = "    ";
-    (0..size).map(|_| INDENT)
-             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
-}
+use xml::name::OwnedName;
+use xml::reader::{EventReader, XmlEvent};
+use xml::writer::{EmitterConfig, EventWriter, Result, XmlEvent as WXMLEvent};
 
 fn main() {
     let file = File::open("./test/test.gpx").unwrap();
     let file = BufReader::new(file);
 
     let parser = EventReader::new(file);
-    let mut depth = 0;
-    let mut treak = vec![];
+    let mut treak: Vec<XmlEvent> = vec![];
     let mut is_treak = false;
     let mut file = File::create("output.xml").unwrap();
-    let mut writer = EmitterConfig::new().perform_indent(true).create_writer(&mut file);
+    let mut writer = EmitterConfig::new()
+        .perform_indent(true)
+        .create_writer(&mut file);
     for e in parser {
         if is_treak {
             match e {
-              Ok(XmlEvent::StartElement { .. }) => {
-                  depth += 1;
-              }
-              Ok(XmlEvent::EndElement { ref name, .. }) => {
-                  depth -= 1;
-                  if &name.local_name == "trk" {
-                      is_treak = false;
-                  }
-              }
-              _ => {}
-            }            
-            treak.push(e.clone());
-            if let Ok(e) = e {
-                if let Some(v) = e.as_writer_event() {
-                    writer.write(v);
+                Ok(XmlEvent::EndElement { ref name, .. }) => {
+                    if &name.local_name == "trk" {
+                        is_treak = false;
+                        treak.reverse();
+                        for ie in treak {
+                            if let Some(v) = ie.as_writer_event() {
+                                writer.write(v).expect("parse failed");
+                            }
+                        }
+                        treak = vec![];
+                    }
                 }
+                Ok(e) => {
+                    treak.push(e);
+                }
+                _ => {}
             }
         } else {
             let e = match e {
-                Ok(XmlEvent::StartElement { name, mut attributes, namespace }) => {
-                    println!("{}+{}", indent(depth), name);                
-                    depth += 1;
+                Ok(XmlEvent::StartElement {
+                    name,
+                    mut attributes,
+                    namespace,
+                }) => {
                     if &name.local_name == "trk" {
                         is_treak = true;
                     }
                     if &name.local_name == "author" {
                         attributes.push(OwnedAttribute::new(OwnedName::local("attr"), "bar"));
                     }
-                    XmlEvent::StartElement { name, attributes, namespace }
+                    XmlEvent::StartElement {
+                        name,
+                        attributes,
+                        namespace,
+                    }
                 }
-                Ok(XmlEvent::EndElement { name }) => {
-                    depth -= 1;
-                    println!("{}-{}", indent(depth), name);
-                    XmlEvent::EndElement { name }
-                }
+                Ok(XmlEvent::EndElement { name }) => XmlEvent::EndElement { name },
                 Ok(a) => a,
-                Err(e) => panic!(e)
+                Err(e) => panic!(e),
             };
             if let Some(v) = e.as_writer_event() {
-                writer.write(v);
+                writer.write(v).expect("parse failed");
             }
         }
     }
